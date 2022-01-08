90ed84fcbc921f3a5e0cd434241d22102bfae70b
Even Pan <yz830620@gmail.com>
2022-01-08 14:58:58 +0800

    add value should be test (major update)

--- stack_queue/test_three_in_one.py
+++ stack_queue/test_three_in_one.py
@@ -3,7 +3,7 @@ from three_in_one import ThreeInOne
 
 @pytest.fixture
 def three_in_one():
-    return ThreeInOne()
+    return ThreeInOne(3)
 
 
 class TestThreeInOne:
@@ -16,3 +16,12 @@ class TestThreeInOne:
 
     def test_stack_is_not_full(self, three_in_one):
         assert three_in_one.stack_is_full(stack_num=3) == False
+
+    def test_add_value_to_stack(self, three_in_one):
+        stack_num = 0
+        value = 5
+        three_in_one.add_value_to_stack(stack_num=stack_num , value=value)
+        assert three_in_one.stacks[stack_num][-1] == value 
+
+    # def test_after_add_not_empty(self, three_in_one):
+    #     assert three_in_one
--- stack_queue/three_in_one.py
+++ stack_queue/three_in_one.py
@@ -2,11 +2,15 @@
 
 
 class ThreeInOne:
-    def __init__(self):
-        pass
+    def __init__(self, stack_qty):
+        self.stack_qty = stack_qty
+        self.stacks = [[]] * stack_qty
 
     def stack_is_empty(self, stack_num):
         return True
 
     def stack_is_full(self, stack_num):
         return False
+
+    def add_value_to_stack(self, stack_num, value):
+        self.stacks[stack_num].append(value)
